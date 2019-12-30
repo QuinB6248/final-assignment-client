f461217d23eb19831b742c35a87132d047d6777b
QuinB6248 <4_sAfJO25N@outlook.com>
2019-12-30 14:13:06 +0100

    CSS change on Form

--- src/actions/ticket.js
+++ src/actions/ticket.js
@@ -3,52 +3,55 @@ const baseUrl = 'http://localhost:4000'
 //const baseUrl ='https://pure-hamlet-15394.herokuapp.com'
 
 
+/////////////////////FETCH TICKET ACTION/////////////////
 export const TICKET_FETCHED = 'TICKET_FETCHED'
 const ticketFetched = (ticket) => ({
   type: TICKET_FETCHED,
   payload: ticket
 })
+
 export const fetchTicket = (id, ticketId) => (dispatch, getState) => {
   if (getState().events) return
+
   request(`${baseUrl}/events/${id}/tickets/${ticketId}/comments`)
-    .then(response => {
-      dispatch(ticketFetched(response.body))})
+    .then(response => dispatch(ticketFetched(response.body)))
     .catch(console.error)
 }
 
-
+//////////////////////CREATE COMMENT ACTION///////////////////
 export const COMMENT_CREATED = 'COMMENT_CREATED'
 const commentCreated = comment => ({
   type: COMMENT_CREATED,
   payload: comment
 })
+
 export const createComment = (id, ticketId, data) => (dispatch, getState) => {
   //const jwt = getState().authUser
   const jwt = sessionStorage.getItem("token")
+
   request
     .post(`${baseUrl}/events/${id}/tickets/${ticketId}/comments`)
     .set('Authorization', `Bearer ${jwt}`)
     .send(data)
-    .then(response => {
-      dispatch(commentCreated(response.body))})
+    .then(response => dispatch(commentCreated(response.body)))
     .catch(console.error)
 }
 
-
+/////////////////////UPDATE TICKET ACTION////////////////////////
 export const TICKET_UPDATED = 'TICKET_UPDATED'
 const ticketUpdated = ticket => ({
   type: TICKET_UPDATED,
   payload: ticket
 })
+
 export const updateTicket = (id, ticketId, data) => (dispatch, getState) => {
   //const jwt = getState().authUser
   const jwt = sessionStorage.getItem("token")
+
   request
     .patch(`${baseUrl}/events/${id}/tickets/${ticketId}`)
     .set('Authorization', `Bearer ${jwt}`)
     .send(data)
-    .then(response => {
-      dispatch(ticketUpdated(response.body))
-    })
+    .then(response => dispatch(ticketUpdated(response.body)))
     .catch(console.error)
 }
